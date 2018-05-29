4065102ac758da821ddf3473bd877344bc24ca86
steven-xie <dev@stevenxie.me>
2018-05-29 00:59:17 -0400

    Refactored code for better maintainability
    The installation code has been separated into modules that are easier to
    maintain, with logical flows that are more 'purely functional'
    (dependent on function input, rather than global variables).

--- tests/installed-correctly.test.js
+++ tests/installed-correctly.test.js
@@ -1,18 +1,18 @@
 const { execFile } = require('child_process');
 
 function showError(msg) {
-  console.error(`Test 'installed-correctly' failed: ${msg}`);
+	console.error(`Test 'installed-correctly' failed: ${msg}`);
 }
 
 execFile('yarn', ['start', '-v'], function(err, stdout) {
-  if (err) {
-    showError(err);
-    process.exit(1);
-  }
-  if (stdout.indexOf('Terraform v') === -1) {
-    showError(
-      `Expected version information from 'terraform -v', instead received: ${stdout}`
-    );
-    process.exit(2);
-  }
+	if (err) {
+		showError(err);
+		process.exit(1);
+	}
+	if (stdout.indexOf('Terraform v') === -1) {
+		showError(
+			`Expected version information from 'terraform -v', instead received: ${stdout}`
+		);
+		process.exit(2);
+	}
 });
