da5a21f5d354941e60fd5180cf46f7d040f31df9
Steven Xie <dev@stevenxie.me>
2018-06-02 09:53:13 -0400

    Patched whitespace inconsistency (no-tabs)

--- src/terraform-wrapper.js
+++ src/terraform-wrapper.js
@@ -11,6 +11,6 @@ const terraform = spawn(command, process.argv.slice(2), { cwd: process.cwd() });
 terraform.stdout.pipe(process.stdout);
 terraform.stderr.pipe(process.stderr);
 terraform.on('error', function(err) {
-	console.error(`Received an error while executing the Terraform binary: ${err}`);
+  console.error(`Received an error while executing the Terraform binary: ${err}`);
   process.exit(1);
 });
